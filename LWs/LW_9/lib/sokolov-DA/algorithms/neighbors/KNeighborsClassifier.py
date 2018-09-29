7c5ef83f36d6d2ed215593ac5a6a5716ecb49056
Vycheslav Sokolov <vycheslavsokolov2013@gmail.com>
2018-09-29 16:43:58 +0600

    init LW # 9

---
+++ LWs/LW_9/lib/sokolov-DA/algorithms/neighbors/KNeighborsClassifier.py
@@ -0,0 +1,13 @@
+class KNeighborsClassifier:
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def fit(self, X, y):
+        pass
+
+    def score(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass

aacf87e62206600fcbc586708b7f2d01829f4d90
Vycheslav Sokolov <vycheslavsokolov2013@gmail.com>
2018-10-26 17:07:40 +0600

    Added dictinary stop words.

--- LWs/LW_10/src/main.py
+++ LWs/LW_10/src/main.py
@@ -1,5 +1,5 @@
 from lib.data_analysis.texts.StopWords import StopWords
 
-sw = StopWords("/home/sokolov/PycharmProjects/plsa/stopwords.dic")
+sw = StopWords("/home/sokolov/PycharmProjects/t/DataAnalysis/lib/data_analysis/texts/stopwords.dic")
 sw.loadStopWordsFromFile()
 print(sw.getStopWords())
